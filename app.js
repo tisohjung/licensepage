3716a760840b2d93e8c0104a430f28225281b252
Administrator <admin@local.host>
2015-09-14 15:28:50 +0900

    port changed to 3001

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen(3000, function() {
-    console.log('server started at port 3000');
+app.listen(3001, function() {
+    console.log('server started at port 3001');
 });
